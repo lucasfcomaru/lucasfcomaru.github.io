0353e51fe76694d1b5dede0846af9dc49fb44fbe
lucasfcomaru <lukscomaru@gmail.com>
2024-08-03 14:30:08 -0300

    Função de validação do formulário

---
+++ js/modules/valida-form.js
@@ -0,0 +1,59 @@
+export default function initValidaForm() {
+  // selecionando o botão
+  const button = document.querySelector("[data-button]");
+
+  function validaForm(event) {
+    // previne o padrão para não enviar o formulário
+    event.preventDefault();
+
+    // seleciona os elementos do formulário
+    const nome = document.querySelector("#nome");
+    const email = document.querySelector("#email");
+    const mensagem = document.querySelector("#mensagem");
+    const aviso = document.querySelector(".contato-aviso");
+    const avisotexto = document.querySelector("[data-aviso]");
+
+    // verifica se todos os campos estão preenchidos
+    if (!nome.value && !email.value && !mensagem.value) {
+      // adiciona a classe ativo ao elemento para mudar o display
+      aviso.classList.add("ativo");
+      // Altera o conteúdo do elemento
+      avisotexto.innerHTML +=
+        "<br>Preencha o campo <strong>nome, email e mensagem</strong>";
+    } else if (!nome.value) {
+      // verifica se o campo nome está preenchido
+      // adiciona a classe ativo ao elemento para mudar o display
+      aviso.classList.add("ativo");
+      // Altera o conteúdo do elemento
+      avisotexto.innerHTML = "Preencha o campo <strong>nome</strong>";
+    } else if (!email.value) {
+      // verifica se o campo email está preenchido
+      // adiciona a classe ativo ao elemento para mudar o display
+      aviso.classList.add("ativo");
+      // Altera o conteúdo do elemento
+      avisotexto.innerHTML = "Preencha o campo <strong>email</strong>";
+    } else if (!email.value.includes("@") || !email.value.includes(".")) {
+      // verifica se contem o @ e . no email
+      // adiciona a classe ativo ao elemento para mudar o display
+      aviso.classList.add("ativo");
+      // Altera o conteúdo do elemento
+      avisotexto.innerHTML = "O email digitado é <strong>inválido</strong>";
+    } else if (!mensagem.value) {
+      // verifica se o campo mensagem está preenchido
+      // adiciona a classe ativo ao elemento para mudar o display
+      aviso.classList.add("ativo");
+      // Altera o conteúdo do elemento
+      avisotexto.innerHTML = "Preencha o campo <strong>mensagem</strong>";
+    } else {
+      // caso todas as condições sejam falsas
+      // adiciona a classe ativo ao elemento para mudar o display
+      aviso.classList.add("ativo");
+      // Mostra uma mensagem de sucesso
+      avisotexto.innerHTML =
+        "<strong>Mensagem enviada com sucesso</strong><br>Nossa equipe está trabalhando para responder o mais rápido possível";
+    }
+  }
+
+  // adicionando evento
+  button.addEventListener("click", validaForm);
+}
--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 import initMenuMobile from './modules/menu-mobile.js';
 import initAnimacao from './modules/animacao-scroll.js';
 import initScrollSuave from './modules/scroll-suave.js';
+import initValidaForm from './modules/valida-form.js';
 
 initMenuMobile();
 initAnimacao();
 initScrollSuave();
+initValidaForm();
