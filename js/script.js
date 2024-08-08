e938c8efab26c43fcb0139af75286c6fc6f1d0bc
lucasfcomaru <lukscomaru@gmail.com>
2024-08-08 15:16:18 -0300

    Formatação do código

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
+// Importa as funções
 import initMenuMobile from './modules/menu-mobile.js';
 import initAnimacao from './modules/animacao-scroll.js';
 import initScrollSuave from './modules/scroll-suave.js';
 import initValidaForm from './modules/valida-form.js';
 import initAnimaNumeros from './modules/anima-numeros.js';
 
+// Inicializa as funções
 initMenuMobile();
 initAnimacao();
 initScrollSuave();
